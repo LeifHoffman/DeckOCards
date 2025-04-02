61bd835d85f598be444c41231a9974026d190032
LeifHoffman <hoffmanl2@etown.edu>
2025-04-02 12:25:24 -0400

    Made card and deck struct

---
+++ card.h
@@ -0,0 +1,11 @@
+/*************************************************************
+ * Purpose: Header file to include card struct and functions
+ * Author: Leif Hoffman
+ * Date Updated: 4-2-2025
+ *************************************************************/
+
+typedef struct Card_struct{
+    char rank;
+    char suit;
+    int value;
+} Card;
--- deck.h
+++ deck.h
@@ -1,6 +1,12 @@
-/*
-* File: deck.h
-* Purpose: This is the header file for all function definitions and structures
-* Author: Leif Hoffman
-* Date Updated: 4-2-2025
-*/
+/********************************************************************************
+ * File: deck.h
+ * Purpose: This is the header file for all function definitions and structures
+ * Author: Leif Hoffman
+ * Date Updated: 4-2-2025
+ ********************************************************************************/
+
+ #include "card.h"
+ 
+typedef struct Deck_struct{
+    Card deck[52];
+} Deck;
